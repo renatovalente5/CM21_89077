47046eb04aa4f62242424fb3d2f3234c905bed89
Renato Valente <renatovalente5@ua.pt>
2021-12-05 00:18:02 +0000

    a funcionar

--- appKotlin/app/src/main/java/pt/ua/appkotlin/MainActivity.kt
+++ appKotlin/app/src/main/java/pt/ua/appkotlin/MainActivity.kt
@@ -1,11 +1,50 @@
 package pt.ua.appkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 
 class MainActivity : AppCompatActivity() {
+    var phoneNo: EditText? = null
+    var callbtn: FloatingActionButton? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        phoneNo = findViewById(R.id.editTextPhone)
+        callbtn = findViewById(R.id.callbtn)
+
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.CALL_PHONE),
+                PERMISSION_CODE
+            )
+        }
+
+        callbtn!!.setOnClickListener(View.OnClickListener {
+            val phoneno = phoneNo!!.getText().toString()
+            val i = Intent(Intent.ACTION_CALL)
+            i.data = Uri.parse("tel:$phoneno")
+            startActivity(i)
+        })
+    }
+
+    companion object {
+        var PERMISSION_CODE = 100
     }
 }
